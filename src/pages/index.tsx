cd2631fa7755f8493d9721292d435d34afa8ce7a
Omkar Arora <aroraomkar@gmail.com>
2023-04-13 22:59:10 +0530

    style: post content size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ const PostView = (props: PostWithUser) => {
           <span className="px-1">·</span>
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
