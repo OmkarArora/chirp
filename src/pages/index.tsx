412e04f7c00cf61aec75fda05b877bae6692f334
Omkar Arora <aroraomkar@gmail.com>
2023-04-15 15:42:55 +0530

    posting loading states + toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ import { RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/Loading";
+import { LoadingPage, LoadingSpinner } from "~/components/Loading";
 import { useState } from "react";
+
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -24,6 +26,15 @@ const CreatePostWizard = () => {
 
       // void tells TS you don't care about the promise, let it run in background
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later");
+      }
     },
   });
 
@@ -44,8 +55,28 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            e.preventDefault();
+
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center ">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
